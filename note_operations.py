018d0e0512cbc03ee5b3a3e6e62f64a96817184f
Evgenii <cujibephoc@gmail.com>
2024-02-22 15:44:44 +0400

    add reate and save func

---
+++ note_operations.py
@@ -0,0 +1,21 @@
+import json
+import os
+from datetime import datatime
+
+def create_note():
+    title = input("Введите заголовок заметки: ")
+    message = input("Введите тело заметки: ")
+    timestamp = datatime.now().strftime('%Y-%m-%d %H:%M:%S')
+    note = {'title': title, 'message': message, 'timestamp': timestamp}
+    save_note(note)
+    return note
+
+def save_note(note):
+    if not os.path.exists('notes.json'):
+        with open('notes.json', 'w') as file:
+            json.dump([], file)
+    with open('notes.json', 'r+') as file:
+        data = json.load(file)
+        data.append(note)
+        file.seek(0)
+        json.dump(data, file, indent=4)
